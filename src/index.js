98a98f23bfd6aa37dcf2814e8a830604ee2edae3
Estefanía <estefaniaramirezmnt@gmail.com>
2024-02-01 18:58:33 +0100

    Added new instruction to the AI

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
 
   let apiKey = "4efbbf43t600f8b07428238a0a4o0852";
   let context =
-    "You are a romantic poem enthusiast and love to writte short poems. Your task is to write a 4 line and separate the 3 first lines with a HTML <br/>. Make sure to follow the user instructions.";
+    "You are a romantic poem enthusiast and love to writte short poems. Your task is to write a 4 line and separate the 3 first lines with a HTML <br/>. If there is not a dot don't write the first letter of the first word of the line with capital letters. Make sure to follow the user instructions.";
   let prompt = `User instructions are: Generate a Spanish poem about: ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
